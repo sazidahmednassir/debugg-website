261c74806e86d2176b6af1d535b34748f8d23598
sazidahmednassir <sazidahmednassir@gmail.com>
2022-03-10 10:28:42 -0800

    fifth commit

--- js/app.js
+++ js/app.js
@@ -160,7 +160,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
